941aa89ba3e353b822885f77e322c6cd348a080e
Corentin MARTIN <coremart@e1r11p17.42.fr>
2021-09-16 13:37:00 +0200

    add TV_LEN

--- ft_ping.h
+++ ft_ping.h
@@ -6,7 +6,7 @@
 /*   By: coremart <coremart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 12:46:55 by coremart          #+#    #+#             */
-/*   Updated: 2021/08/31 13:00:45 by coremart         ###   ########.fr       */
+/*   Updated: 2021/09/14 15:56:53 by coremart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,11 @@
 	#define IS_LINUX (0)
 #endif
 
+struct tv32 {
+	u_int32_t tv32_sec;
+	u_int32_t tv32_usec;
+};
+
+# define TV_LEN (sizeof(struct tv32))
 
 #endif
